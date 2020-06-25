65d275e9bcae561190b2152ff2c3da0abbb0390a
신진현 <shinjinhyun@sinjinhyeon-ui-MacBookPro.local>
2020-06-25 23:00:39 +0900

    컴포넌트 생명주기

--- src/App.js
+++ src/App.js
@@ -1,69 +1,47 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({ name, picture, rating }){
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
-    rating: 3
-  },
-  {
-    id: 2,
-    name: 'Kimchi2',
-    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
-    rating: 5
-  },
-  {
-    id: 3,
-    name: 'Kimchi3',
-    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
-    rating: 4
-  },
-  {
-    id: 4,
-    name: 'Kimchi4',
-    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
-    rating: 3.3
-  },
-  {
-    id: 5,
-    name: 'Kimchi5',
-    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
-    rating: 3.8
-  },
-  {
-    id: 6,
-    name: 'Kimchi6',
-    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
-    rating: 2
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('Hello');
   }
-];
+  state = {
+    count: 0
+  };
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.Image} rating={dish.rating} />
-      ))}
-    </div>
-  );
+  add = () => {
+    this.setState(current => ({
+      count : current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    this.setState(current => ({
+      count : current.count - 1,
+    }));
+  };
+
+  componentDidMount(){
+    console.log('DidMount');
+  }
+
+  componentDidUpdate(){
+    console.log('DidUpdate');
+  }
+
+  componentWillUnmount(){
+    console.log('Unmount');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-Food.prototype = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
 
 export default App;
