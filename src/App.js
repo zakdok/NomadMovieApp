c6251a8d9b64a21d1cc476075730d496def0c281
신진현 <shinjinhyun@sinjinhyeon-ui-MacBookPro.local>
2020-06-25 20:54:52 +0900

    음식 앱 실습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
+function Food({ name, picture, rating }){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
     </div>
   );
 }
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
+    rating: 3
+  },
+  {
+    id: 2,
+    name: 'Kimchi2',
+    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
+    rating: 5
+  },
+  {
+    id: 3,
+    name: 'Kimchi3',
+    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
+    rating: 4
+  },
+  {
+    id: 4,
+    name: 'Kimchi4',
+    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
+    rating: 3.3
+  },
+  {
+    id: 5,
+    name: 'Kimchi5',
+    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
+    rating: 3.8
+  },
+  {
+    id: 6,
+    name: 'Kimchi6',
+    Image: 'https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/026/550/original/IMG_2236s.jpg?2019&d=750x400',
+    rating: 2
+  }
+];
+
+function App() {
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.Image} rating={dish.rating} />
+      ))}
+    </div>
+  );
+}
+
+Food.prototype = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 export default App;
